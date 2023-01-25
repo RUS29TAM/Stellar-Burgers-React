ab57e6071542301fae40b8ad64c02f921b616b08
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-01-25 23:20:52 +0300

    chore: add icon in input

--- src/components/Forms/FormProfile/FormProfile.jsx
+++ src/components/Forms/FormProfile/FormProfile.jsx
@@ -13,12 +13,16 @@ const FormProfile = () => {
   return (
     <div className={styleProfile.container}>
       <form className={styleProfile.form}>
-        <Input type="text" placeholder="Имя" onChange={onNameChange} value={name}/>
-        <EmailInput type="email" placeholder="Логин" onChange={onEmailChange} value={email}/>
-        <PasswordInput type="password" placeholder="Пароль" onChange={onPasswordChange}
-                       value={password}/>
-        <Button type="primary" value="Войти" htmlType={"reset"}>Отмена</Button>
-        <Button type="primary" value="Войти" htmlType={"submit"}>Сохранить</Button>
+        <Input className={styleProfile.inputIcon} type="text" placeholder="Имя" onChange={onNameChange} value={name}/>
+        <EmailInput className={styleProfile.inputIcon} type="email" placeholder="Логин" onChange={onEmailChange} value={email}/>
+        <PasswordInput type="password" placeholder="Пароль" onChange={onPasswordChange} value={password}/>
+        <div className={styleProfile.btnWrapper}>
+          {/*<a className={`text text_type_main-default ${styleReg.link}`}*/}
+          {/*   href="src/Pages/PageLogin/PageLogin#">Отмена</a>*/}
+          <Button type="secondary" value="Войти" htmlType={"reset"}>Отмена</Button> {/*по макету должна быть кнопка, но тогда в PixelPerfect не попадаю, шинина кнопок
+           "primary" и "secondary" не позволяет добиться совпадения*/}
+          <Button type="primary" value="Войти" htmlType={"submit"}>Сохранить</Button>
+        </div>
       </form>
     </div>
   );
