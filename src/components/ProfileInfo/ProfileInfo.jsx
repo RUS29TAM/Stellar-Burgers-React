307fb8064d1a5e354fe3b868ba268ca0f0dbf87d
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-01-26 23:14:53 +0300

    chore: minor changes

--- src/components/ProfileInfo/ProfileInfo.jsx
+++ src/components/ProfileInfo/ProfileInfo.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 import styleProfileInfo from './ProfileInfo.module.css'
 import {NavLink} from "react-router-dom";
 
-const setActiveLink = ({isActive}) => isActive ? 'active-link' : 'inactive-link';
+const setActiveLink = ({isActive}) => isActive ? `${styleProfileInfo.link} text_color_primary` : `${styleProfileInfo.link} text_color_inactive`;
 
 const ProfileInfo = () => {
   return (
     <div className={`text text_color_primary text_type_main-medium ${styleProfileInfo.wrapper}`}>
-      <NavLink className={setActiveLink}  to="/profile" >Профиль</NavLink>
+      <NavLink className={setActiveLink} to="/profile" >Профиль</NavLink>
       <NavLink className={setActiveLink} to="/orders">История заказов</NavLink>
       <NavLink className={setActiveLink} to="/">Выход</NavLink>
     </div>
