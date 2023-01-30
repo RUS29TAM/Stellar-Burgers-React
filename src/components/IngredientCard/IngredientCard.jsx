e639ab770130d7862e2885b62d04fd114d6295ba
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-01-31 02:09:19 +0300

    chore: move the modal window call condition to AppMain

--- src/components/IngredientCard/IngredientCard.jsx
+++ src/components/IngredientCard/IngredientCard.jsx
@@ -1,36 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 import styleCard from '../IngredientCard/IngredientCard.module.css'
 import {CurrencyIcon, Counter} from '@ya.praktikum/react-developer-burger-ui-components';
-import IngredientDetails from '../IngredientDetails/IngredientDetails';
 import ingredientType from '../../utils/types';
-import Modal from "../Modal/Modal";
 import {useDrag} from "react-dnd";
 import PropTypes from "prop-types";
-import {useDispatch, useSelector} from "react-redux";
-import {SET_INGREDIENT} from "../../services/actions/ingredientDetails";
 import {Link} from "react-router-dom";
 
-
 const IngredientCard = ({ingredient, getIngredientCount}) => {
-  const [isIngredientDetailsPopupOpen, setIngredientDetailsPopupOpen] = useState(false);
-  const handleIngredientSetOpen = value => setIngredientDetailsPopupOpen(value)
-
   const [, dragRef] = useDrag({
     type: 'ingredientCard',
     item: ingredient,
   });
 
-  const dispatch = useDispatch()
-  const ingredientDetails = useSelector(state => state.ingredientDetails.ingredient)
-
-  const handleIngredientsPopupOpen = () => {
-    dispatch({type: SET_INGREDIENT, payload: ingredient})
-    setIngredientDetailsPopupOpen(true)
-  };
-
   return (
-    <div ref={dragRef} className={styleCard.card} onClick={handleIngredientsPopupOpen}>
-      <Link to={`/ingredients/${ingredient._id}`} className={"text_color_primary"} replace={true} state={{ingredient: ingredient, from: '/', stateModal: true}} >
+    <div ref={dragRef} className={styleCard.card}>
+      <Link to={`/ingredients/${ingredient._id}`} className={"text_color_primary"} replace={true}
+            state={{ingredient: ingredient, from: '/', stateModal: true}}>
       <span className={styleCard.span}>
       <img className={`mr-4 ml-4`} src={ingredient.image} alt={ingredient.name}/>
       <div className={`mt-1 mb-1 ${styleCard.price}`}>
@@ -42,14 +27,6 @@ const IngredientCard = ({ingredient, getIngredientCount}) => {
       </div>
         ({getIngredientCount(ingredient._id) !== 0 &&
         <Counter count={getIngredientCount(ingredient._id)} size="default"/>})
-
-        ({isIngredientDetailsPopupOpen && ingredientDetails &&
-        <Modal
-          setOpen={handleIngredientSetOpen}
-        >
-          <IngredientDetails/>
-        </Modal>
-      })
       </span>
       </Link>
     </div>
