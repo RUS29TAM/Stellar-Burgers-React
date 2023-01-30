127583534448949c6076f94bd4e385f5e32a09ec
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-01-31 02:41:05 +0300

    chore: change the condition for PageIngredientsId

--- src/Pages/PageIngregientsId/PageIngredientsId.jsx
+++ src/Pages/PageIngregientsId/PageIngredientsId.jsx
@@ -1,31 +1,41 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, } from 'react';
 import AppHeader from "../../components/AppHeader/AppHeader";
 import IngredientDetails from "../../components/IngredientDetails/IngredientDetails";
 import {useLocation, useParams} from "react-router-dom";
 import api from "../../components/Api/Api";
 import PageMain from "../PageMain/PageMain";
+import {useDispatch, useSelector} from "react-redux";
+import {SET_INGREDIENT} from "../../services/actions/ingredientDetails";
 
 const PageIngredientsId = () => {
   const location = useLocation()
   const {id} = useParams()
-  const [ingredient, setIngredient] = useState(null)
+  const dispatch = useDispatch()
+  const ingredient = useSelector(state => state.ingredientDetails.ingredient)
 
   useEffect(() => {
     if (location.state?.from !== '/') {
       api.getIngredients()
         .then(ingredients => {
           const ingredient = ingredients.data.filter(ingredient => ingredient._id === id)[0]
-          setIngredient(ingredient)
+          dispatch({type: SET_INGREDIENT, payload: ingredient})
         })
-        .catch(error => console.log(error.message))
+        .catch(error => console.log(error))
     }
   }, [id, location.state])
 
-  return (location.state?.from !== '/' ? <PageMain/> : ingredient ?
+  return (location.state?.from === '/'
+      ?
+        <PageMain/>
+      :
+      ingredient !== null
+        ?
         <>
           <AppHeader/>
           <IngredientDetails/>
-        </> : ''
+        </>
+        :
+        'loading...'
   );
 };
 
