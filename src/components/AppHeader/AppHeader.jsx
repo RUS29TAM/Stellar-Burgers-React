badc0866cf5c2419879c7fa6df752d2b2e2126f6
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-01-31 17:22:46 +0300

    chore: draft add an active link to the header menu

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,7 +1,7 @@
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 import stylesHeader from './AppHeader.module.css';
-import {Link} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 const AppHeader = () => {
   return (
@@ -10,20 +10,20 @@ const AppHeader = () => {
         <nav className={`navigationList ${stylesHeader.navigationList} mb-4`}>
           <ul className={`${stylesHeader.navigationList} pl-3`}>
             <li>
-              <Link to="/" className={`navigationLink ${stylesHeader.navigationLink} pt-4 pr-5`}>
+              <NavLink activeClassName={stylesHeader.navigationLink_active} to="/" className={`navigationLink ${stylesHeader.navigationLink} pt-4 pr-5`}>
                 <BurgerIcon type="secondary"/>
                 <span className="text text_type_main-default text_color_inactive ml-2">
                   Конструктор
                 </span>
-              </Link>
+              </NavLink>
             </li>
             <li>
-              <Link to="/orders" className={`navigationLink ${stylesHeader.navigationLink} pt-4 pl-5`}>
+              <NavLink activeClassName={stylesHeader.navigationLink_active} to="/feed" className={`navigationLink ${stylesHeader.navigationLink} pt-4 pl-5`}>
                 <ListIcon type="secondary"/>
                 <span className="text text_type_main-default text_color_inactive ml-2">
                   Летна заказов
                 </span>
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
@@ -31,12 +31,12 @@ const AppHeader = () => {
           <Logo/>
         </div>
         <div className={`personalAccount ${stylesHeader.personalAccount} pr-8`}>
-          <Link className={`${stylesHeader.navigationLink}`} to="/profile">
+          <NavLink activeClassName={stylesHeader.navigationLink_active} className={`${stylesHeader.navigationLink}`} to="/profile">
             <ProfileIcon type="secondary"/>
             <span className="text text_type_main-default text_color_inactive ml-2">
               Личный кабинет
             </span>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </header>
