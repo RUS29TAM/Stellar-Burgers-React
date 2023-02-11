3a318dd4f0cbe1f3bd7c443c42f90491b7eaf7cf
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-02-12 01:32:59 +0300

    chore: create a separate AppHeaderNavigationItem component

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -1,9 +1,8 @@
-import {BurgerIcon, ListIcon, Logo, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import {Logo} from '@ya.praktikum/react-developer-burger-ui-components';
 import React from 'react';
 import stylesHeader from './AppHeader.module.css';
-import {NavLink} from "react-router-dom";
+import AppHeaderNavigationItem from "../AppHeaderNavigationItem/AppHeaderNavigationItem";
 
-const setActiveLink = ({isActive}) => isActive ? `${stylesHeader.navigationLink} pt-4 pr-5 text_color_primary` : `${stylesHeader.navigationLink} pt-4 pr-5 text_color_inactive`;
 const AppHeader = () => {
   return (
     <header className={`header ${stylesHeader.header}`}>
@@ -11,20 +10,10 @@ const AppHeader = () => {
         <nav className={`navigationList ${stylesHeader.navigationList} mb-4`}>
           <ul className={`${stylesHeader.navigationList} pl-3`}>
             <li>
-              <NavLink to="/" className={setActiveLink}>
-                <BurgerIcon type="secondary"/>
-                <span className={`${stylesHeader.span} text text_type_main-default ml-2`}>
-                  Конструктор
-                </span>
-              </NavLink>
+              <AppHeaderNavigationItem iconComponentName="BurgerIcon" to={'/'}>Конструктор</AppHeaderNavigationItem>
             </li>
             <li>
-              <NavLink className={setActiveLink} to="/feed">
-                <ListIcon type="secondary"/>
-                <span className={`${stylesHeader.span} text text_type_main-default ml-2`}>
-                  Летна заказов
-                </span>
-              </NavLink>
+              <AppHeaderNavigationItem iconComponentName="ListIcon" to={'/feed'}>Лента заказов</AppHeaderNavigationItem>
             </li>
           </ul>
         </nav>
@@ -32,12 +21,7 @@ const AppHeader = () => {
           <Logo/>
         </div>
         <div className={`personalAccount ${stylesHeader.personalAccount} pr-8`}>
-          <NavLink className={setActiveLink} to="/profile">
-            <ProfileIcon type="secondary"/>
-            <span className={`${stylesHeader.span} text text_type_main-default ml-2`}>
-              Личный кабинет
-            </span>
-          </NavLink>
+          <AppHeaderNavigationItem iconComponentName="ProfileIcon" to={'/profile'}>Личный кабинет</AppHeaderNavigationItem>
         </div>
       </div>
     </header>
