59577df7487fa783eb28261addf0b6ba198ff058
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-02-12 01:33:20 +0300

    chore: create a separate AppHeaderNavigationItem component

---
+++ src/components/AppHeaderNavigationItem/AppHeaderNavigationItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {NavLink, useMatch} from "react-router-dom";
+import {BurgerIcon, ListIcon, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import stylesHeader from "../AppHeader/AppHeader.module.css";
+
+const AppHeaderNavigationItem = ({to, children, iconComponentName}) => {
+  const isActive = useMatch(to)
+  const setActiveLink = ({isActive}) => isActive ? `${stylesHeader.navigationLink} pt-4 pr-5 text_color_primary` : `${stylesHeader.navigationLink} pt-4 pr-5 text_color_inactive`;
+  const getIcon = () => {
+    if (iconComponentName === 'BurgerIcon') {
+      return <BurgerIcon type={isActive ? "primary" : "secondary"}/>
+    }
+    if (iconComponentName === 'ListIcon') {
+      return <ListIcon type={isActive ? "primary" : "secondary"}/>
+    }
+    if (iconComponentName === 'ProfileIcon') {
+      return <ProfileIcon type={isActive ? "primary" : "secondary"}/>
+    }
+  }
+
+  return (<NavLink to={to} className={setActiveLink}>
+    {iconComponentName && getIcon()}
+    <span className={`${stylesHeader.span} text text_type_main-default ml-2`}>
+        {children}
+      </span>
+  </NavLink>);
+};
+
+export default AppHeaderNavigationItem;
