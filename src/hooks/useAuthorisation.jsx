9bfa3a78ca53940ab879fc881fedacbf97997107
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-02-12 01:40:16 +0300

    chore: add action

--- src/hooks/useAuthorisation.jsx
+++ src/hooks/useAuthorisation.jsx
@@ -3,7 +3,7 @@ import {useSelector} from "react-redux";
 function useAuthorisation() {
   const user = useSelector(state => state.userReducer)
 
-  return {name: user.name, email: user.email, isAuth: !!user.email}
+  return {name: user.name, email: user.email, isAuth: !!user.email, accept: user.accept}
 }
 
 export default useAuthorisation;
