19ab080e679ecaa9aa0a08ea3d1195974624748a
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-02-12 01:37:05 +0300

    chore: added an amplifier

---
+++ src/services/actions/checkAuthorizedThunk.jsx
@@ -0,0 +1,10 @@
+import useUserController from "../../hooks/useUserController";
+import {userAcceptAction, userErrorAction, userLoadingAction} from "./user";
+
+export const checkAuthorizedThunk = () => (dispatch) => {
+  const userController = useUserController()
+  dispatch(userLoadingAction())
+  userController.checkAuth()
+    .then(user => dispatch(userAcceptAction(user)))
+    .catch(error => dispatch(userErrorAction(error.message)))
+}
