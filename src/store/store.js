0673c94e6ce692eee8140d10ef9f25645e4d6243
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-02-16 18:15:18 +0300

    chore: rename index on store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
-import rootReducer from "./reducers/rootReducer";
+import rootReducer from "../services/reducers/rootReducer";
 
 export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
