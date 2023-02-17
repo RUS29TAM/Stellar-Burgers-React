5b7a6d02150b8abc49ee71426fe6f67302f0215a
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-02-17 16:36:06 +0300

    chore: add draft wsOrdersFeedAction

--- src/services/actions/wsOrdersFeedsAction.jsx
+++ src/services/actions/wsOrdersFeedsAction.jsx
@@ -1,15 +1,14 @@
 export const WS_ORDERS_FEEDS_CONNECT = 'WS_ORDERS_FEEDS_CONNECT'                 //соединять
-export const WS_ORDERS_FEEDS_CONNECTING = 'WS_ORDERS_FEEDS_CONNECTING'           //связывающий
-export const WS_ORDERS_FEEDS_CONNECTED = 'WS_ORDERS_FEEDS_CONNECTED'             //связанный
+export const WS_ORDERS_FEEDS_CONNECTING = 'WS_ORDERS_FEEDS_CONNECTING'           //соединяющий
 export const WS_ORDERS_FEEDS_DISCONNECT = 'WS_ORDERS_FEEDS_DISCONNECT'           //разъединять
 export const WS_ORDERS_FEEDS_DISCONNECTING = 'WS_ORDERS_FEEDS_DISCONNECTING'     //разъединенный
+export const WS_ORDERS_FEEDS_ERR = 'WS_ORDERS_FEEDS_ERR'                         //err
 export const WS_ORDERS_FEEDS_GET_MESSAGE = 'WS_ORDERS_FEEDS_GET_MESSAGE'         //получить сообщение
 
-export const wsOrdersFeedsConnectAction = url => ({type: WS_ORDERS_FEEDS_CONNECT, payload: url})
-export const wsOrdersFeedsConnectingAction = e => ({type: WS_ORDERS_FEEDS_CONNECTING, payload: e})
-export const wsOrdersFeedsConnectedAction = e => ({type: WS_ORDERS_FEEDS_CONNECTED, payload: e})
-
+export const wsOrdersFeedsConnectAction = host => ({type: WS_ORDERS_FEEDS_CONNECT, host})
+export const wsOrdersFeedsConnectingAction = host => ({type: WS_ORDERS_FEEDS_CONNECTING, host})
 export const wsOrdersFeedDisconnectAction = () => ({type: WS_ORDERS_FEEDS_DISCONNECT})
-export const wsOrdersFeedsDisconnectingAction = e => ({type: WS_ORDERS_FEEDS_DISCONNECTING, payload: e})
-export const wsOrdersFeedsGetMessageAction = data => ({type: WS_ORDERS_FEEDS_GET_MESSAGE, payload: data})
+export const wsOrdersFeedsDisconnectingAction = host => ({type: WS_ORDERS_FEEDS_DISCONNECTING, host})
+export const wsOrdersFeedsErrAction = host => ({type: WS_ORDERS_FEEDS_ERR, host})
+export const wsOrdersFeedsGetMessageAction = host => ({type: WS_ORDERS_FEEDS_GET_MESSAGE, host})
 
