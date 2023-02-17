aa0ed0cdb5ad80953c44acec482cc03014e40029
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-02-17 16:48:47 +0300

    chore: add in store draft WebSocket OrdersFeeds

--- src/store/store.jsx
+++ src/store/store.jsx
@@ -2,5 +2,6 @@ import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from "../services/reducers/rootReducer";
+import wsOrdersFeeds from "../middleware/wsOrdersFeeds";
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, wsOrdersFeeds(),)));
