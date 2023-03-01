1df7ead5c858507fca45a96a4fea5a39950a0ff4
RUS29TAM <rustam.nikonchuk@yandex.ru>
2023-03-02 00:53:26 +0300

    chore: nothing significant

--- src/components/HalfBun/HalfBun.jsx
+++ src/components/HalfBun/HalfBun.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {ConstructorElement} from '@ya.praktikum/react-developer-burger-ui-components';
 import PropTypes from 'prop-types';
-import ingredientType from '../../utils/types';
+import ingredientType from '../../utils/ingredientType';
 
 const HalfBun = ({type, bun}) => {
 
