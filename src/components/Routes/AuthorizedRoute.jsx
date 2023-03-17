027495b83200a16a33025db865e545832d93f59f
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-17 14:58:57 +0300

    refactoring: replacement useDispatch with AppDispatch

--- src/components/Routes/AuthorizedRoute.jsx
+++ src/components/Routes/AuthorizedRoute.jsx
@@ -3,12 +3,12 @@ import useAuthorisation from "../../hooks/useAuthorisation";
 import {Navigate} from "react-router-dom";
 import PropTypes from "prop-types";
 import PreLoader from "../PreLoader/PreLoader";
-import {useDispatch} from "react-redux";
 import {checkAuthorizedThunk} from "../../services/thunks/checkAuthorizedThunk";
+import {AppDispatch} from "../../hooks/appDispatch";
 
 const AuthorizedRoute = ({children}) => {
     const {isAuth, accept, error} = useAuthorisation()
-    const dispatch = useDispatch()
+    const dispatch = AppDispatch()
 
     useEffect(() => dispatch(checkAuthorizedThunk()), [])
     return (
