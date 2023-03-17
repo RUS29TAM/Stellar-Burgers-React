768d0435e15b5fad9369bcae16db60ec8b727449
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-17 15:01:32 +0300

    chore: fix the import

--- src/services/thunks/checkAuthorizedThunk.ts
+++ src/services/thunks/checkAuthorizedThunk.ts
@@ -1,6 +1,6 @@
 import useUserController from "../../hooks/useUserController";
 import {userAcceptAction, userErrorAction, userLoadingAction} from "../actions/userAction";
-import {TAppThunk} from "./TAppThunk";
+import {TAppThunk} from "../../types/TAppThunk";
 import {userAction} from "../../types/TUser";
 
 export const checkAuthorizedThunk = (): TAppThunk<userAction> => (dispatch) => {
