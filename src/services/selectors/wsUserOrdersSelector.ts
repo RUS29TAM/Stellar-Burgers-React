ad856041903e279a89fec501863bccb5305bfda1
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-17 15:27:59 +0300

    refactoring: changing the file extension to tsx

--- src/services/selectors/wsUserOrdersSelector.ts
+++ src/services/selectors/wsUserOrdersSelector.ts
@@ -1,5 +1,6 @@
 import {createSelector} from "@reduxjs/toolkit";
-export const wsUserOrdersReducerSelectorDefault = (state) => state.wsUserOrderReducer
-export const wsUserOrderSelectorDefault = (state) => state.wsUserOrderReducer.orders
+import {RootState} from "../../store/store";
+export const wsUserOrdersReducerSelectorDefault = (state: RootState) => state.wsUserOrderReducer
+export const wsUserOrderSelectorDefault = (state: RootState) => state.wsUserOrderReducer.orders
 export const wsUserOrdersReducerSelectorModified = createSelector(wsUserOrdersReducerSelectorDefault, state => state)
 export const wsUserOrderSelectorModified = createSelector(wsUserOrderSelectorDefault, state => state)
