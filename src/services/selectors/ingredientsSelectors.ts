770cc16010fa3a7ff727942584d0a4bca626b6fb
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-17 15:09:53 +0300

    refactoring: TypeScript for the ingredientsSelectors

--- src/services/selectors/ingredientsSelectors.ts
+++ src/services/selectors/ingredientsSelectors.ts
@@ -1,7 +1,8 @@
 import {createSelector} from "@reduxjs/toolkit";
+import {RootState} from "../../store/store";
 
-export const ingredientsSelectorReducer = state => state.ingredients
-export const ingredientsSelectorDefault = state => state.ingredients.data
+export const ingredientsSelectorReducer = (state: RootState) => state.ingredients
+export const ingredientsSelectorDefault = (state: RootState) => state.ingredients.data
 export const ingredientsSelectorReducerModified = createSelector(ingredientsSelectorReducer, state => (state))
 export const ingredientsSelectorModified = createSelector(ingredientsSelectorDefault, state => state)
 
