57901e9e37a95994ab47e1ec95b555c09f0ca304
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-22 18:07:30 +0300

    refactoring: TypeScript for the AuthorizedRoute

--- src/components/Routes/AuthorizedRoute.tsx
+++ src/components/Routes/AuthorizedRoute.tsx
@@ -1,23 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {FC, ReactElement, ReactNode, useEffect} from 'react';
 import useAuthorisation from "../../hooks/useAuthorisation";
 import {Navigate} from "react-router-dom";
-import PropTypes from "prop-types";
 import PreLoader from "../PreLoader/PreLoader";
 import {checkAuthorizedThunk} from "../../services/thunks/checkAuthorizedThunk";
 import {AppDispatch} from "../../hooks/appDispatch";
 
-const AuthorizedRoute = ({children}) => {
+interface IAuthorizedRoute {
+    children: ReactNode
+}
+
+const AuthorizedRoute: FC<IAuthorizedRoute> = ({children}) => {
     const {isAuth, accept, error} = useAuthorisation()
     const dispatch = AppDispatch()
 
     useEffect(() => dispatch(checkAuthorizedThunk()), [])
     return (
-        !accept ? error ? <Navigate to='/login'/> : <PreLoader/> : isAuth ? children : <Navigate to='/login'/>
+        !accept ? error ? <Navigate to='/login'/> : <PreLoader/> : isAuth ? children as ReactElement : <Navigate to='/login'/>
     );
 };
 
-AuthorizedRoute.propTypes = {
-    children: PropTypes.element.isRequired
-}
 
 export default AuthorizedRoute;
