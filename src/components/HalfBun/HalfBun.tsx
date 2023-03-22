edc52e431cb46c89aaba0882ac2b9c9e99d88848
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-22 18:09:34 +0300

    refactoring: TypeScript for the HalfBun

--- src/components/HalfBun/HalfBun.tsx
+++ src/components/HalfBun/HalfBun.tsx
@@ -2,11 +2,11 @@ import React, {FC} from 'react';
 import {ConstructorElement} from '@ya.praktikum/react-developer-burger-ui-components';
 import {IIngredient} from "../../interfaces/data/IIngredient";
 
-interface IProps {
+interface IHalfBun {
     type: 'top' | 'bottom',
     bun: IIngredient
 }
-const HalfBun: FC<IProps> = ({type, bun}) => {
+const HalfBun: FC<IHalfBun> = ({type, bun}) => {
 
     return (
         <div className='ml-8 mr-4'>
