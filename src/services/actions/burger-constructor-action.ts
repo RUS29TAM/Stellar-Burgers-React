2a4b377edfedd0deb469aff4bfb08dbb7558d4fc
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-23 17:37:52 +0300

    refactor: replace `any` & `unknown`

--- src/services/actions/burger-constructor-action.ts
+++ src/services/actions/burger-constructor-action.ts
@@ -1,8 +1,9 @@
 import {nanoid} from "nanoid";
 import {TBurgerConstructorActions, TBurgerConstructor} from "../../types/t-burger-constructor";
+import {IIngredient} from "../../interfaces/data/i-ingredient";
 
-export const addBun = (payload: unknown): { payload: unknown; type: TBurgerConstructor.ADD_BUN } => ({type: TBurgerConstructor.ADD_BUN, payload});
-export const addFilling = (payload: unknown): TBurgerConstructorActions => ({type: TBurgerConstructor.ADD_FILLING, id: nanoid(), payload});
-export const removeFilling = (payload: any): TBurgerConstructorActions => ({type: TBurgerConstructor.REMOVE_FILLING, payload});
+export const addBun = (payload: IIngredient | null): TBurgerConstructorActions => ({type: TBurgerConstructor.ADD_BUN, payload});
+export const addFilling = (payload: IIngredient): TBurgerConstructorActions => ({type: TBurgerConstructor.ADD_FILLING, id: nanoid(), payload});
+export const removeFilling = (payload: IIngredient): TBurgerConstructorActions => ({type: TBurgerConstructor.REMOVE_FILLING, payload});
 export const swapFillings = (payload: { from: any; to: any; }): TBurgerConstructorActions => ({type: TBurgerConstructor.SWAP_FILLINGS, payload});
 
