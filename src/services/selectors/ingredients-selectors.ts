de33da0d48260795a36c694f264f474aef65d1f0
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-27 16:29:32 +0300

    refactor: add :IIngredientsReducer['data'] in ingredientsSelectorDefault

--- src/services/selectors/ingredients-selectors.ts
+++ src/services/selectors/ingredients-selectors.ts
@@ -1,8 +1,9 @@
 import {createSelector} from "@reduxjs/toolkit";
 import {RootState} from "../../store/store";
+import {IIngredientsReducer} from "../../interfaces/data/i-ingredients-reducer";
 
 export const ingredientsSelectorReducer = (state: RootState) => state.ingredients
-export const ingredientsSelectorDefault = (state: RootState) => state.ingredients.data
+export const ingredientsSelectorDefault = (state: RootState):IIngredientsReducer['data'] => state.ingredients.data
 export const ingredientsSelectorReducerModified = createSelector(ingredientsSelectorReducer, state => (state))
 export const ingredientsSelectorModified = createSelector(ingredientsSelectorDefault, state => state)
 
