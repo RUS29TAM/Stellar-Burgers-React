ff40202b0e0386e4b8ae4d82e299469793dd6fc7
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2023-03-29 16:43:27 +0300

    refactor: replace `any` swapFillings burger-constructor-action.ts

--- src/services/actions/burger-constructor-action.ts
+++ src/services/actions/burger-constructor-action.ts
@@ -5,5 +5,5 @@ import {IIngredient} from "../../interfaces/data/i-ingredient";
 export const addBun = (payload: IIngredient | null): TBurgerConstructorActions => ({type: TBurgerConstructor.ADD_BUN, payload});
 export const addFilling = (payload: IIngredient): TBurgerConstructorActions => ({type: TBurgerConstructor.ADD_FILLING, id: nanoid(), payload});
 export const removeFilling = (payload: IIngredient): TBurgerConstructorActions => ({type: TBurgerConstructor.REMOVE_FILLING, payload});
-export const swapFillings = (payload: { from: any; to: any; }): TBurgerConstructorActions => ({type: TBurgerConstructor.SWAP_FILLINGS, payload});
+export const swapFillings = (payload: { from: number; to: any; }): TBurgerConstructorActions => ({type: TBurgerConstructor.SWAP_FILLINGS, payload});
 
